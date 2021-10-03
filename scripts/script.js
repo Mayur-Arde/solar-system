8769e854fdd6cda5fe4ad822074036e595dbb1a5
Mayur-Arde <mayurarde8@gmail.com>
2021-10-03 16:11:16 +0530

    UI done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 'use strict';
+const selectEl = document.getElementById('select');
+const optionEl = document.getElementById('option');
+
+
+selectEl.addEventListener('click', function(){
+  // selectEl.style.transform = "rotate(7deg)"; 
+});
