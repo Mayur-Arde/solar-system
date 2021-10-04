903a2f95ecee861092a636098b8fb91cfbf5f5d5
Mayur-Arde <mayurarde8@gmail.com>
2021-10-04 23:28:24 +0530

    show container refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,29 @@
 'use strict';
+// declare variables
 const inputEl = document.getElementById('mass');
 const selectEl = document.getElementById('select');
-const optionEl = document.getElementById('option');
 const btnEl = document.querySelector('.btn');
 const messageEl = document.querySelector('.message');
 const planetImgEl = document.querySelector('.planet-image');
 const planetTextEl = document.querySelector('.planet');
 const weightOutputEl = document.getElementById('weight');
 
+// show and hide variable
 const flexContainer = document.querySelector('.flex-container');
 const description = document.querySelector('.description');
 const planetImg = document.querySelector('.planet-image');
 const resultCircle = document.querySelector('.result-circle ');
 
-let planet = selectEl.value;
+// programm variabel 
+let planet;
 let weight;
-// let input = inputEl.value;
 
+// FUNCTIONS
+
+// function to show all result 
 const showResult = function () {
   console.log(planet);
   let input = inputEl.value;
-  // let gravity;
   if (inputEl.value == '' || inputEl.value == null) {
     showContainers();
     messageEl.innerHTML = `Mass is required`;
@@ -28,81 +31,53 @@ const showResult = function () {
     showContainers();
     messageEl.innerHTML = `You did not select planet yet.`;
   } else if (inputEl.value > 0) {
+    showContainers();
+    showImg();
     if (planet === 'mercury') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet); 
       weight = 0.377 * input;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }else if (planet === 'venus') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 0.91 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'earth') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 1 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'moon') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 	0.166 * inputEl.value; 
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'mars') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 	0.38 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'jupiter') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 2.34 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'saturn') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 1.06 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'uranus') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 0.92 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'neptune') {
-      let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 1.19 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
     }
     else if (planet === 'pluto') {
-      // let input = inputEl.value;
-      showContainers();
-      showImg();
       showPlanet(planet);
       weight = 0.06 * inputEl.value;
       weightOutputEl.innerHTML = `${weight.toFixed(2)} N`;
@@ -110,22 +85,27 @@ const showResult = function () {
   }
 };
 
+// function to show img and name on planet accordingly
 const showPlanet = function (planet) {
   planetImg.src = `./assests/images/${planet}.png`;
   messageEl.innerHTML = `The weight of the object on ${planet}`;
 };
 
+// function to show containers
 const showContainers = function () {
   flexContainer.style.display = 'flex';
   description.style.display = 'block';
 };
 
+// function to show img and weight on planet 
 const showImg = function () {
   planetImg.style.display = 'block';
   resultCircle.style.display = 'flex';
 };
 
+//Add Event Listeners
 selectEl.addEventListener('change', (e) => {
   planet = e.target.value;
-  // showResult();
 });
+
+btnEl.addEventListener('click', showResult);
